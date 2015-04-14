90775c0c3e4c581ad0f6b5fc6c6ffefc76c3dd4a
tenchhos <tenchhos@localhost>
2015-04-13 22:39:36 -0400

    unittest for tackle.py completed

--- app/battleball/game_cli/test_tackle.py
+++ app/battleball/game_cli/test_tackle.py
@@ -9,6 +9,38 @@ the function that calls it.
 
 import unittest
 from tackle import tackle
-from game_pieces import game_piece
+
+'''
+Two instances of game piece (player) are created.
+Each with different roll size (dice) and position.
+Then initially, both the players aren't injured.
+But, after the two players tackle, one gets injured
+(outta game) and other is not (keep playing).
+'''
+class test_tackle(unittest.TestCase):
+
+	def test_tackle_function(self):
+		# instantiate two game piece objects
+		player1 = game_piece(20, 5, 'lineman')
+		player2 = game_piece(8, 17, 'running back')
+	
+		# make sure both the players are not injured initially
+		self.assertEqual(0, player1.injured)
+		self.assertEqual(0, player2.injured)
+
+		# now let the two players tackle by calling tackle function
+		# return player is one who lose the tackle
+		playerInjured = tackle(player1, player2)	
+
+		# now set the returned player's injured attribute as True
+		playerInjured.injured = 1
+
+		# make sure the player who lost tackle is injured and other is not
+		if playerInjured == player1:
+			self.assertEqual(1, player1.injured)
+			self.assertEqual(0, plauer2.injured)
+		else:
+			self.assertEqual(1, player2.injured)
+			self.assertEqual(0, player1.injured)
 
 
