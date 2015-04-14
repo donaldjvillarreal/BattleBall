a35f8130f29fc8b38958d48085b2dd4037461a1c
tenchhos <tenchhos@localhost>
2015-04-13 20:10:15 -0400

    test code that respects the pylint test

--- app/battleball/game_cli/test_handoff.py
+++ app/battleball/game_cli/test_handoff.py
@@ -1,9 +1,14 @@
-# This is the test code for the ball handoff function
-# between two players: 'handoff.py'
-# Test code is incomplete ... 
+'''
+This is the unittest code for the ball handoff function between 
+two players which is in source file called 'handoff.py'.
+To handover the ball from one player to another, the handoff
+function must be used. Each player's dice is rolled, and if 
+the resulting number is different, then player A passes the 
+ball to player B; otherwise, resolve fumble function is called.
+'''
 
 '''
-#######uncomment this to print some command line test#######
+####### uncomment this to print some command line test #######
 ############################################################
 from game_pieces import game_piece
 from handoff import handoff
@@ -24,10 +29,17 @@ print 'player2 has ball? ', player2.has_ball
 '''
 
 import unittest
-from mock import Mock, patch
 from handoff import handoff
 from game_pieces import game_piece
 
+'''
+Two instances of game piece (player) are created.
+Each with differnt roll size (dice) and position.
+Then initially, both the player doesn't have the ball.
+But, then one gets the ball with ball_toggle(), and 
+handsoff to the other player. The test assumes
+dice rolls results differently.
+'''
 class test_handoff(unittest.TestCase):
 	
 	def test_handoff_function(self):
@@ -37,7 +49,7 @@ class test_handoff(unittest.TestCase):
 		
 		# both player shouldn't have ball by default
 		self.assertEqual(0, player1.has_ball)
-		self.assertEqual(0, palyer2.has_ball)
+		self.assertEqual(0, player2.has_ball)
 
 		# suppose player 1 has the ball, but player 2 doesn't
 		player1.ball_toggle()
