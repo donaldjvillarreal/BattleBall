1d04ac298bf9c2a3f46793d3fb9dd2d200d26924
Donald Villarreal <donaldjvillarreal@gmail.com>
2015-04-22 21:50:40 -0400

    function added to convert x and y mouse coordinates to col/row positions.

--- app/app/static/battleball/js/game_board.js
+++ app/app/static/battleball/js/game_board.js
@@ -20,21 +20,23 @@ var long_row = 5;
 var hsqSize = sqSize/2;
 var short_row = long_row-1;
 
-
+/*
 var arr = [     ['E', 'E', 'E', 'E', 'E', 'E'],
-                ['0h', 'E', 'E', '1h', 'E', 'E'],
-                ['E', '2h', 'E', '4h', 'E'],
-                ['6h', 'E', '9h', 'E', '10h', 'E'],
-                ['E', 'E', 'E', 'E', 'X'],
+                ['E', 'E', 'E', 'E', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E'],
                 ['E', 'E', 'E', 'E', 'E', 'E'],
                 ['E', 'E', 'B', 'E', 'E'],
                 ['E', 'E', 'E', 'E', 'E', 'E'],
                 ['E', 'E', 'E', 'E', 'E'],
-                ['6a', 'E', '9a', 'E', '10a', 'E'],
-                ['E', '2a', 'E', '4a', 'E'],
-                ['Oa', 'E', 'E', '1a', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E'],
+                ['E', 'E', 'E', 'E', 'E', 'E'],
                 ['E', 'E', 'E', 'E', 'E', 'E']      ];
+*/
 
+document.addEventListener("DOMContentLoaded", draw, false);
 
 //This is the main function and will paint in the football field
 function draw() {
@@ -43,7 +45,20 @@ function draw() {
     var ctx = canvas.getContext('2d');
 
     gameboard(ctx, arr);
+    canvas.addEventListener("mousedown", getPosition, false);
   }
+}
+
+//Returns the coordinates of the mouse
+function getPosition(event) {
+    var canvas = document.getElementById("canvas");
+    x = event.x;
+    y = event.y;
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    clickpos = position(x-border, y-border)
+
+    alert("x: " + clickpos.col + "  y: " + clickpos.row);
 }
 
 //This function will create a board with no pieces on it
@@ -56,11 +71,29 @@ function gameboard(ctx) {
         for (j = 0; j <= long_row; j++) move(i, j, arr[i][j], ctx);
 
 }
+
+//This function will take in the desired spot and piece indicator to move the piece to that location
+function move(col, row, ind, ctx) {
+    ctx.strokeStyle = 'white';
+    var team = ind.charAt(ind.length-1)
+    if (col === 0 && ind =='E') ctx.fillStyle = '#09D';
+    else if (col == field_width && ind =='E') ctx.fillStyle = '#F55';
+    else if (ind == 'E') ctx.fillStyle = '#6C0';
+    else if (ind == 'B') ctx.fillStyle = 'yellow';
+    else if (ind == 'X') ctx.fillStyle = 'black';
+    else if (team == 'h') ctx.fillStyle = 'blue';
+    else if (team == 'a') ctx.fillStyle = 'red';
+
+    var piece = coordinates(col, row);
+    ctx.fillRect(border + piece.x, border + piece.y, sqSize, piece.size);
+    ctx.strokeRect(border + piece.x, border + piece.y, sqSize, piece.size);
+}
+
 //This function will take in a row and column.
 //The columns start from the left and the rows start from the bottom.
 //The output is the x, y coordinates of the upper left pixel of the square and the size of the square
 function coordinates(col, row) {
-  var o = new Object({});
+    var o = new Object({});
     o.x= sqSize*col;
     if(col%2 !== 0 || col === 0 || col === field_width) {
         if(row==long_row) {
@@ -81,19 +114,14 @@ function coordinates(col, row) {
     return o;
 }
 
-//This function will take in the desired spot and piece indicator to move the piece to that location
-function move(col, row, ind, ctx) {
-    ctx.strokeStyle = 'white';
-    var team = ind.charAt(ind.length-1)
-    if (col === 0 && ind =='E') ctx.fillStyle = '#09D';
-    else if (col == field_width && ind =='E') ctx.fillStyle = '#F55';
-    else if (ind == 'E') ctx.fillStyle = '#6C0';
-    else if (ind == 'B') ctx.fillStyle = 'yellow';
-    else if (ind == 'X') ctx.fillStyle = 'black';
-    else if (team == 'h') ctx.fillStyle = 'blue';
-    else if (team == 'a') ctx.fillStyle = 'red';
-
-    var piece = coordinates(col, row);
-    ctx.fillRect(border + piece.x, border + piece.y, sqSize, piece.size);
-    ctx.strokeRect(border + piece.x, border + piece.y, sqSize, piece.size);
+function position(posx, posy) {
+    var o = new Object({});
+    o.col = Math.floor(posx/sqSize);
+    var yfix = -(posy - long_row*sqSize);
+    if(o.col%2 !== 0 || o.col === 0 || o.col === field_width) {
+        if(yfix < hsqSize) o.row = 0;
+        else o.row = Math.floor((yfix+hsqSize)/sqSize);
+    }
+    else o.row = Math.floor(yfix/sqSize);
+    return o;
 }
