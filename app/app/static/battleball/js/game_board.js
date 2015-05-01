7cc11ede4545477e48eaae0850abb84a6f990c9b
Donald Villarreal <donaldjvillarreal@gmail.com>
2015-05-01 14:34:18 -0400

    fixed issues found using jshint.

--- app/app/static/battleball/js/game_board.js
+++ app/app/static/battleball/js/game_board.js
@@ -8,25 +8,28 @@ window.onload = function(){
     //}
     //arr = json.game.board;
     draw();
-}
+};
 
 //These variables can be changed to create a bigger/smaller field
 var field_width = 12,
     sqSize = 100,
     border = 25,
     long_row = 5,
-    pieces = 3,
     selectedPiece = null,
     ctx = null,
+    canvas = null,
     HOME_TEAM = 0,
     AWAY_TEAM = 1,
     tackle = false,
+    pieces = new Image(),
     moves = -1,
     home_pieces = 2,
     away_pieces = 2,
     home_score = 0,
     away_score = 0,
     touchdown = false,
+    fumble = false,
+    fumble_move = 2,
     currentTurn = HOME_TEAM;
 
 //these variables should not be changed
@@ -86,10 +89,9 @@ document.addEventListener("DOMContentLoaded", draw, false);
 
 //This is the main function and will paint in the football field
 function draw() {
-    var canvas = document.getElementById('canvas');
+    canvas = document.getElementById('canvas');
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
-        pieces = new Image();
         pieces.src = '/static/battleball/images/pieces.png';
         gameboard();
         canvas.addEventListener("click", getPosition, false);
@@ -101,7 +103,8 @@ function draw() {
 
 function print_turn() {
     clear_print_turn();
-    var mytext = "Turn: "
+    var turn;
+    var mytext = "Turn: ";
     if (currentTurn === 0) {
         turn = "Home Team";
         ctx.fillStyle="blue";
@@ -127,11 +130,11 @@ function print_move() {
     else ctx.fillStyle="red";
     mytext += moves;
     ctx.font = "30px Arial";
-    ctx.fillText(mytext, border+400, long_row*sqSize+sqSize)
+    ctx.fillText(mytext, border+400, long_row*sqSize+sqSize);
 }
 
 function clear_print_move() {
-    ctx.fillStyle = "white"
+    ctx.fillStyle = "white";
     ctx.fillRect(border+400, long_row*sqSize+hsqSize,300,100);
 }
 
@@ -149,7 +152,8 @@ function getPosition(event) {
     if (selectedPiece === null) {
         checkIfPieceClicked(clickedBlock);
     }
-    else if(tackle) processTackle(clickedBlock);
+    else if (tackle) processTackle(clickedBlock);
+    else if (fumble) processFumble(clickedBlock);
     // if there is a selected piece, move it
     else if (moves >= 0) {
         print_move();
@@ -172,15 +176,16 @@ function roll(roll_size) {
     /*
     This function creates a random number from 1- roll size
    */
-    proll = Math.floor((Math.random() * roll_size) + 1);
+    var proll = Math.floor((Math.random() * roll_size) + 1);
     return proll;
 }
 
 //This function will create a board
 function gameboard() {
     // fill in long rows
-    for (var i = 1; i <= (field_width-1); i+=2)
-        for (var j = 0; j <= long_row; j++) fill_space(i, j, arr[i][j]);
+    var i, j;
+    for (i = 1; i <= (field_width-1); i+=2)
+        for (j = 0; j <= long_row; j++) fill_space(i, j, arr[i][j]);
     // fill in short rows
     for (i = 2; i <= (field_width-1); i+=2)
         for (j = 0; j <= short_row; j++) fill_space(i, j, arr[i][j]);
@@ -292,7 +297,6 @@ function position(posx, posy) {
 
     var board_bottom = long_row*sqSize+border;
     var yfix = -(posy - border - long_row*sqSize);
-    var edge_right = (field_width+1)*sqSize+border;
     if(posy <= board_bottom && posy >= border) {
         if(o.col%2 !== 0 || o.col === 0 || o.col === field_width) {
             if(yfix < hsqSize) o.row = 0;
@@ -312,11 +316,12 @@ function checkIfPieceClicked(clickedBlock) {
 }
 
 function getPieceAtBlock(clickedBlock) {
+    var team;
     if(tackle){
-        var team = (currentTurn === HOME_TEAM ? away : home);
+        team = (currentTurn === HOME_TEAM ? away : home);
     }
     else{
-        var team = (currentTurn === HOME_TEAM ? home : away);
+        team = (currentTurn === HOME_TEAM ? home : away);
     }
 
     return getPieceAtBlockForTeam(team, clickedBlock);
@@ -405,8 +410,9 @@ function canSelectedMoveToBlock(selectedPiece, clickedBlock)
 }
 
 function adjacentEnemy(selectedPiece, clickedBlock) {
-    var occupied = surroundingSpaces(selectedPiece);
-    var occounter, nextMove=[], bNextRowEmpty;
+    var enemy,
+        occupied = surroundingSpaces(selectedPiece),
+        occounter, nextMove=[];
     
     nextMove.col = selectedPiece.position.xpos;
     nextMove.row = selectedPiece.position.ypos;
@@ -453,8 +459,7 @@ function movePiece(clickedBlock) {
     // TODO: Tackle function    
 
     // update piece object to match board status, possibly it's own function later
-    var team = (currentTurn === AWAY_TEAM ? away : home),
-        opposite = (currentTurn !== AWAY_TEAM ? away : home);
+    var team = (currentTurn === AWAY_TEAM ? away : home);
 
     team[selectedPiece.itr].position.xpos = clickedBlock.col;
     team[selectedPiece.itr].position.ypos = clickedBlock.row;
@@ -467,7 +472,7 @@ function movePiece(clickedBlock) {
         var adj_ind = arr[space.col][space.row];
         console.log(adj_ind);
         if(adj_ind !== 'E' && adj_ind !== 'B' && adj_ind !== 'X'){
-            var team = adj_ind.charAt(adj_ind.length-1);
+            team = adj_ind.charAt(adj_ind.length-1);
             console.log(team); 
             if(team !== cur_team){
                 tackle = true;
@@ -475,17 +480,16 @@ function movePiece(clickedBlock) {
         }
     }
 
-    if (currentTurn === HOME_TEAM && selectedPiece.has_ball == true && clickedBlock.col == field_width)
-        process_touchdown(HOME_TEAM);
+    if (currentTurn === HOME_TEAM && selectedPiece.has_ball === true && clickedBlock.col === field_width)
+        processTouchdown(HOME_TEAM);
 
-    else if (currentTurn === AWAY_TEAM && selectedPiece.has_ball == true && clickedBlock.col == 0)
-        process_touchdown(AWAY_TEAM);
+    else if (currentTurn === AWAY_TEAM && selectedPiece.has_ball === true && clickedBlock.col === 0)
+        processTouchdown(AWAY_TEAM);
 
-    if(tackle === false && moves === 0 && touchdown === false) {
+    if(tackle === false && moves === 0 && touchdown === false && fumble === false) {
         clear_print_move();
         selectedPiece = null;
         moves = -1;
-        number_pieces = count_pieces();
         if (home_pieces === 0) currentTurn = AWAY_TEAM;
         else if (away_pieces === 0) currentTurn = HOME_TEAM;
         else currentTurn = (currentTurn === AWAY_TEAM ? HOME_TEAM : AWAY_TEAM);
@@ -554,25 +558,15 @@ function surroundingSpaces(selectedPiece){
         else if(row == long_row) occupied.push(dl, d, dr);
         else occupied.push(u, ul, dl, d, dr, ur);
     }
-    return occupied
+    return occupied;
 }
 
 function pickupBall(piece){
     // This function allows a piece to pick up
     // the game ball
-    piece['has_ball'] = true;
+    piece.has_ball = true;
 }
 
-function count_pieces() {
-    home_pieces = 0;
-    away_pieces = 0;
-    for (iPieceCounter = 0; iPieceCounter < 2; iPieceCounter++) {
-        if(home[iPieceCounter].injured === 0)
-            home_pieces += 1;
-        if(away[iPieceCounter].injured === 0)
-            away_pieces += 1;
-    }
-}
 function processTackle(clickedBlock){
     //This function checks whether the clicked block has an enemy piece
     // Then resolves the tackle
@@ -581,15 +575,15 @@ function processTackle(clickedBlock){
     var enemy_piece = getPieceAtBlock(clickedBlock),
         neighbor = adjacentEnemy(selectedPiece, clickedBlock);
     if(enemy_piece !== null && neighbor === true){
-        tackle_roll = roll(selectedPiece.roll_size);
-        defend_roll = roll(enemy_piece.roll_size);
+        var tackle_roll = roll(selectedPiece.roll_size),
+            defend_roll = roll(enemy_piece.roll_size);
         alert('your piece rolled a ' + tackle_roll + ' the opposing team rolled a ' + defend_roll);
         //if tackling piece wins
         if(tackle_roll < defend_roll){
             if(tackle_roll === 1){
                 enemy_piece.injured = 2;
             }
-            if(enemy_piece.has_ball == true) {
+            if(enemy_piece.has_ball === true) {
                 selectedPiece.has_ball = true;
                 enemy_piece.has_ball = false;
                 fill_space(selectedPiece.position.xpos, selectedPiece.position.ypos,
@@ -605,7 +599,7 @@ function processTackle(clickedBlock){
             if(defend_roll === 1){
                 selectedPiece.injured = 2;
             }
-            if(selectedPiece.has_ball == true) {
+            if(selectedPiece.has_ball === true) {
                 enemy_piece.has_ball = true;
                 selectedPiece.has_ball = false;
                 fill_space(enemy_piece.position.xpos, enemy_piece.position.ypos,
@@ -626,15 +620,16 @@ function processTackle(clickedBlock){
                 enemy_piece.injured = 1;
                 }
             fill_space(selectedPiece.position.xpos,selectedPiece.position.ypos, 'X');
-            fill_space(enemy_piece.position.xpos,enemy_piece.position.ypos, 'X')
+            fill_space(enemy_piece.position.xpos,enemy_piece.position.ypos, 'X');
             arr[clickedBlock.col][clickedBlock.row] = 'X';
             arr[selectedPiece.position.xpos][selectedPiece.position.ypos] = 'X';
+            fumble = true;
+            fumble_move = 2;
         }
         tackle = false;
         selectedPiece = null;
         moves = -1;
         clear_print_move();
-        number_pieces = count_pieces();
         if (home_pieces === 0) currentTurn = AWAY_TEAM;
         else if (away_pieces === 0) currentTurn = HOME_TEAM;
         else currentTurn = (currentTurn === AWAY_TEAM ? HOME_TEAM : AWAY_TEAM);
@@ -642,7 +637,8 @@ function processTackle(clickedBlock){
     }
 }
 
-function process_touchdown (team) {
+function processTouchdown (team) {
+    var i;
     if (team === HOME_TEAM) home_score++;
     else if (team === AWAY_TEAM) away_score++;
 
@@ -688,7 +684,17 @@ function process_touchdown (team) {
     }
 
     moves = 0;
-    currentTurn = ((home_score+away_score)%2 === 0 ? AWAY_TEAM : HOME_TEAM);
+    currentTurn = ((home_score+away_score+1)%2 === 0 ? AWAY_TEAM : HOME_TEAM);
     draw();
+}
+
+function processFumble(clickedBlock) {
+    if (selectedPiece.has_ball === false)
+        currentTurn = (currentTurn === AWAY_TEAM ? HOME_TEAM : AWAY_TEAM);
+
+    var emptySpace = canSelectedMoveToBlock(selectedPiece, clickedBlock);
+    arr[clickedBlock.col][clickedBlock.row] = 'B';
+    arr[selectedPiece.position.xpos][selectedPiece.position.ypos] = 'E';
+    fumble_move-=1;
 
 }
