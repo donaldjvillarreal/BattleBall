33cebb806e47a428bdc23c385eac6ae3b861806b
calvin <calvin@localhost>
2015-05-04 04:33:29 +0000

    acceptance test for homepage

--- app/acceptance_test.py
+++ app/acceptance_test.py
@@ -8,7 +8,7 @@ class BattleballPageTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_registration(self):
+    def test_home_page(self):
         # opening the page
         self.browser.get('http://localhost:8000/battleball/')
 
@@ -17,6 +17,56 @@ class BattleballPageTest(unittest.TestCase):
 
         # navigation bar
         anchors = self.browser.find_elements_by_css_selector("a");
+        self.assertIn('Home', anchors[0].text)
+        self.assertIn('About', anchors[1].text)
+        self.assertIn('Contact', anchors[2].text)
+        self.assertIn('Register', anchors[3].text)
+        self.assertIn('Login', anchors[4].text)
+
+    #def test_about_page(self):
+        # opening the page
+        #self.browser.get('http://localhost:8000/battleball/')
+
+        # title of the page
+        #self.assertIn('Battleball', self.browser.title)
+
+        # navigation bar
+        #anchors = self.browser.find_elements_by_css_selector("a");
+
+        # about page
+        #anchors[1].click()
+
+    #def test_contact_page(self):
+        # opening the page
+        #self.browser.get('http://localhost:8000/battleball/')
+
+        # title of the page
+        #self.assertIn('Battleball', self.browser.title)
+
+        # navigation bar
+        #anchors = self.browser.find_elements_by_css_selector("a");
+
+        # contact page
+        #anchors[2].click()
+
+    def test_registration_page(self):
+        # opening the page
+        self.browser.get('http://localhost:8000/battleball/')
+
+        # title of the page
+        self.assertIn('Battleball', self.browser.title)
+
+        # navigation bar
+        anchors = self.browser.find_elements_by_css_selector("a");
+
+        # home page
+        #anchors[0].click()
+
+        # about page
+        #anchors[1].click()
+
+        # contact page
+        #anchors[2].click()
 
         # registration page
         anchors[3].click()
@@ -43,9 +93,18 @@ class BattleballPageTest(unittest.TestCase):
         errorslist = self.browser.find_elements_by_class_name("errorlist")
         self.assertIn("This field is required.", errorslist[1].text)
 
-    #def test_game_list_page(self):
-        #self.browser.get('http://localhost:8000/battleball/board')
-        #self.assertIn('Django', self.browser.title)
+    def test_login_page(self):
+        # opening the page
+        self.browser.get('http://localhost:8000/battleball/')
+
+        # title of the page
+        self.assertIn('Battleball', self.browser.title)
+
+        # navigation bar
+        anchors = self.browser.find_elements_by_css_selector("a");
+
+        # login
+        anchors[4].click()
 
 if __name__ == '__main__':
     unittest.main()
