e50681d2cf2d96a8ca50c73c058198c7a6b52a6e
Donald Villarreal <donaldjvillarreal@gmail.com>
2015-05-13 21:26:53 -0400

    More pylint cleanup.  up to 9.20

--- app/battleball/game_cli/handoff.py
+++ app/battleball/game_cli/handoff.py
@@ -37,10 +37,3 @@ def handoff(player1, player2):
             print
             player1.ball_toggle()
             player2.ball_toggle()
-
-    else:
-        '''
-        if results of dice rolls are same,
-        then resolve fumble fucntion is called,
-        which is yet to be implemented.
-        '''
--- app/battleball/test_handoff.py
+++ app/battleball/test_handoff.py
@@ -7,32 +7,11 @@ the resulting number is different, then player A passes the
 ball to player B; otherwise, resolve fumble function is called.
 '''
 
-'''
-####### uncomment this to print some command line test #######
-############################################################
-from game_pieces import game_piece
-from handoff import handoff
-import random
-
-player1 = game_piece(6, 13, 'running back')
-player2 = game_piece(20, 1, 'linemam 1')
-
-# suppose player1 has the ball, and player2 doesn't
-player1.ball_toggle()
-print 'player1 has ball? ', player1.has_ball
-print 'player2 has ball? ', player2.has_ball
-
-# now test the handoff function
-handoff(player1, player2)
-print 'player1 has ball? ', player1.has_ball
-print 'player2 has ball? ', player2.has_ball
-'''
-
 import unittest
 from battleball.game_cli.handoff import handoff
 from battleball.game_cli.game_pieces import game_piece
 
-class TEST_HANDOFF(unittest.TestCase):
+class TESTHANDOFF(unittest.TestCase):
     '''
     Two instances of game piece (player) are created. Each with differnt roll size (dice) and
     position. Then initially, both the player doesn't have the ball. But, then one gets the ball
