03e5a842a73b69176bc37a82750677b3b08b51ba
Donald Villarreal <donaldjvillarreal@gmail.com>
2015-05-13 21:04:32 -0400

    more pylint cleanup

--- app/battleball/test_tackle.py
+++ app/battleball/test_tackle.py
@@ -1,43 +1,44 @@
 '''
 This is the unittest code for the tackle function which
-is implemented in 'tackel.py'. This function takes two inputs 
+is implemented in 'tackel.py'. This function takes two inputs
 as game piece objects. The function should rolls their dice
 and compares which one is smaller. The smaller one is considered
-injured and it's out of the game. The losing piece is returned to 
+injured and it's out of the game. The losing piece is returned to
 the function that calls it.
 '''
 
 import unittest
-from game_cli.tackle import tackle
-from game_cli.game_pieces import game_piece
+from battleball.game_cli.tackle import tackle
+from battleball.game_cli.game_pieces import game_piece
 
-'''
-Two instances of game piece (player) are created.
-Each with different roll size (dice) and position.
-Then initially, both the players aren't injured.
-But, after the two players tackle, one gets injured
-(outta game) and other is not (keep playing).
-'''
-class test_tackle(unittest.TestCase):
-
+class TESTTACKLE(unittest.TestCase):
+    '''
+    Two instances of game piece (player) are created.
+    Each with different roll size (dice) and position.
+    '''
     def test_tackle_function(self):
+        '''
+        Then initially, both the players aren't injured.
+        But, after the two players tackle, one gets injured
+        (outta game) and other is not (keep playing).
+        '''
         # instantiate two game piece objects
         player1 = game_piece(20, 5, 'lineman')
         player2 = game_piece(8, 17, 'running back')
-    
+
         # make sure both the players are not injured initially
         self.assertEqual(0, player1.injured)
         self.assertEqual(0, player2.injured)
 
         # now let the two players tackle by calling tackle function
         # return player is one who lose the tackle
-        playerInjured = tackle(player1, player2)    
+        playerinjured = tackle(player1, player2)
 
         # now set the returned player's injured attribute as True
-        playerInjured.injured = 1
+        playerinjured.injured = 1
 
         # make sure the player who lost tackle is injured and other is not
-        if playerInjured == player1:
+        if playerinjured == player1:
             self.assertEqual(1, player1.injured)
             self.assertEqual(0, player2.injured)
         else:
